dfde322ff5b735721565210759f1b806beb47461
anildalar <dollorinfotech@gmail.com>
2021-03-15 09:51:01 +0530

    t14

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 //1. Import Area
 const express = require('express');
 const app = express();
+
 const env = require('dotenv');
-var bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoute');
 const adminRoutes = require('./routes/admin/adminRoute');
 const categoryRoutes = require('./routes/category');
@@ -13,16 +13,16 @@ const mongoose = require('mongoose');
 
 env.config();
 
-//   http://project:4000/api/getsometing
-//Middleware
+app.use(express.json());
 
-//app.use(express.json());
-app.use(bodyParser.json());
 
 app.use('/api',userRoutes);
 app.use('/api',adminRoutes);
 
 app.use('/api',categoryRoutes);
+
+
+
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -54,6 +54,7 @@ mongoose.connect(
             });
 
 //user Middleware
+
 
 app.listen(process.env.PORT,function(){
     console.log(`Server is running on port ${process.env.PORT}`);
--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const { auth, isAdminToken } = require('../common-middleware');
 const categoryModel = require("../models/categoryModel");
 var router = express.Router();
 
-router.post('/category/create',function(req,res,next){
+router.post('/category/create',auth,isAdminToken,function(req,res,next){
     
     //Destructure
     const { name,slug,parentId } = req.body;
@@ -19,5 +20,22 @@ router.post('/category/create',function(req,res,next){
     });
 });
 
+router.post('/category/delete',auth,isAdminToken,async function(req,res){
+    //req.body.something
+    console.log(req.body._id);
+
+    const deleteCategory = await categoryModel.findOneAndDelete({
+        _id:req.body._id
+    });
+    if(deleteCategory){
+        res.status(200).json({
+            msg:"Category Deleted Successfully"
+        });
+    }else{
+        res.status(401).json({
+            msg:"Unable to Deleted Category"
+        });
+    }
+  });
 
 module.exports = router;
